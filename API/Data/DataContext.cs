80cab7e92fa40941ccafee0091bd2944d2278296
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2023-07-03 12:30:00 +0200

    Commit

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -8,5 +8,5 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions options) : base(options)
     {
     }
-    public DbSet<AppUser> MyProperty { get; set;}
+    public DbSet<AppUser> Users { get; set;}
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities;
 
 public class AppUser
 {
+    [Key]
     public int IdUser { get;  set; }
+    
     public string UserName { get;  set; }
     
     
